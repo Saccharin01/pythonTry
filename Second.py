5ae778538bb0e8290534f6b16f138f82ba236478
조우식 <chousik01@gmail.com>
2024-07-22 10:36:36 +0900

    first Python

---
+++ Second.py
@@ -0,0 +1,21 @@
+fruits = ["apple","banana","cherry"]
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+
+age = 19
+if (age >= 18):
+    print("you are an adult")
+else:
+    print("yor are a minor")
+
+for i in range(5):
+    print(1)
+
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1
---
+++ firstPY.py
@@ -0,0 +1,10 @@
+x = 10;
+y = 3.15;
+name = "alice"
+is_student = True;
+convert = int(name)
+age = "25";
+age = int(age)
+print(age)
+print(x+y)
+print(name+y)
